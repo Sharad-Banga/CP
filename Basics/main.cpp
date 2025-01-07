e6b3688e64854fbb86365bc3afe29a1bbfc7107b
Sharad Banga <sharadiit03@gmail.com>
2025-01-07 13:16:42 +0530

    update

---
+++ Basics/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  string name;
+  int age;
+  cin>>name>>age;
+
+  cout<<"name : "<<name<<"\n";
+  cout<<"age : "<<age;
+  return 0;
+}
