6b7d04fc8f52e274f0c29aa00fccef3f3c7d090b
Sharad Banga <sharadiit03@gmail.com>
2025-01-14 05:04:55 +0530

    update

---
+++ CF/cf2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nitinFastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
+
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+
+typedef long long int ll;
+typedef vector<vector<char>> vvc;
+
+int Frog(int x,int y,int k){
+
+  
+
+}
+
+void solve() {
+    
+    int x,y,k;
+    cin>>x>>y>>k;
+
+  cout<<Frog(x,y,k);
+  
+    
+}
+
+int main() {
+    nitinFastIO;
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
