c34c088dd07df0f3d0d1a23d019dcb4e02a0cb4b
Sharad Banga <sharadiit03@gmail.com>
2025-01-21 20:13:23 +0530

    update

---
+++ CF/cf6.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nitinFastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
+
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define pb push_back
+#define all(v) v.begin(), v.end()
+
+typedef long long int ll;
+typedef vector<vector<char>> vvc;
+
+void solve() {
+
+  long long n;
+  cin>>n;
+  long long ans = 1;
+  while(n>0){
+    if(n==1){
+      ans*=1;
+    }else{
+      ans*=2;
+    }
+    
+    n=(int)n/4;
+  }
+  
+  cout<<ans<<endl;
+  
+    
+}
+
+int main() {
+    nitinFastIO;
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
