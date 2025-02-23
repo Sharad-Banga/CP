80540b7754cfad124751a2abe216ad258b5fe7dc
Sharad Banga <sharadiit03@gmail.com>
2025-02-24 04:33:03 +0530

    update

---
+++ prime_1/prime1.cpp
@@ -0,0 +1,48 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<(b); i+=k)
+#define RFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define RFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int  ull;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+
+bool isPrime(int n){
+
+  FOR(i,2,n){
+    if(n%i==0){
+      return false;
+    }
+  }
+  return true;
+}
+
+
+void solve(){
+  
+  int n ;
+  cin>>n;
+
+  cout<<isPrime(n)<<endl;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    FOR(i,1,t+1) {
+        solve();
+    }
+    return 0;
+}
